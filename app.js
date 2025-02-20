5bc4a345a96f91ad0a45f48db520d56dcba5b321
GuYujiFujimoto <yujifujimoto@outlook.com.br>
2025-02-20 11:36:06 -0300

    aumentar numero secreto

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -20,7 +20,7 @@ function adicionarTexto(tag, texto) {
 
 function exibirMensagemInicial() {
     adicionarTexto('h1', 'jogo do número secreto');
-    adicionarTexto('p', 'Escolha um número entre 1 e 10');   
+    adicionarTexto('p', 'Escolha um número entre 1 e 100');   
 }
 
 exibirMensagemInicial();
